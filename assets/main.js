9bedc71ba0599edeffc5706c3ff377b8ea1e885f
Carlos <carlao_epalha@hotmail.com>
2018-10-18 02:55:13 -0300

    Aplicando materialize

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,15 @@
 $(document).ready(function () {
-    $(".cross").hide();
-    $(".submenu").hide();
-    $(".hamburger").click(function () {
-        $(".submenu").slideToggle("slow", function () {
-            $(".hamburger").hide();
-            $(".cross").show();
-        });
-    });
-
-    $(".cross").click(function () {
-        $(".submenu").slideToggle("slow", function () {
-            $(".cross").hide();
-            $(".hamburger").show();
-        });
-    });
-
-    $(".submenu a").click(function(){
+    $(".sidenav a").click(function () {
         var txt = this.text;
-        if(txt.length > 19){
-            txt = txt.substring(0,19) + ' ...';
+        if (txt.length > 19) {
+            txt = txt.substring(0, 19) + ' ...';
         }
         $('#title').html(txt);
     });
 
-    $(".nav-item").click(function(){
-        $(".nav-item").removeClass("active");
+    $(".tab").click(function () {
+        $(".tab").removeClass("active");
         $(this).addClass("active");
     });
-
+    $('#title').html($(".sidenav li.active > a").text());
 });
